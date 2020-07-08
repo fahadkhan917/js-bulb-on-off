50785f946ecf9918ebdc150754d9c3909aafaed9
fahadkhan917 <fk16181@gmail.com>
2020-07-08 23:29:47 +1000

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+function on(){
+
+var ele=document.getElementById("img");
+ele.src="on.jpg";
+
+}
+
+
+function off(){
+
+    var ele=document.getElementById("img");
+    ele.src="off.jpg";
+    
+    }
